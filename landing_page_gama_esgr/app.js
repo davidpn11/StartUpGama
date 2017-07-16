28e9b54d98cd3ae0b8ffc2dad4e87d5b48b21362
David Nascimento <davidpn.11@gmail.com>
2017-07-16 16:14:06 -0300

    node app

---
+++ landing_page_gama_esgr/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+app = express();
+
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  res.render("index");
+});
+
+app.listen(3000,function () {
+    console.log("Escutando na porta 3000...");
+});
