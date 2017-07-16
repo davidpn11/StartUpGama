6349f48042b1d2284c2d951b91dfe1813ebd0b4f
David Nascimento <davidpn.11@gmail.com>
2017-07-15 21:14:57 -0300

    Backend Added

---
+++ back_end/blogdata-app.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient,
+    assert = require('assert');
+var myParser = require("body-parser");
+var app = express();
+
+//app.use(myParser.urlencoded({extended : true}));
+app.use(myParser.json());
+
+
+MongoClient.connect('mongodb://localhost:27017/blog-data', function(err, db) {
+
+
+    assert.equal(err, null);
+    console.log("Successfully connected to MongoDB.");
+    
+
+  app.put("/leed", function(request, response) {
+       
+       try {
+      
+       var json = request.body;
+       console.log(JSON.stringify(json));        
+       // delete json._id;
+
+       if(json.nome_completo || json.email){
+            db.collection("leeds").insertOne(json, function(err, res) {
+                console.log("Inserted document with _id: " + res.insertedId + "\n");
+                json._id = res.insertedId;
+            });
+       }else{
+        throw new Exception;
+       }
+
+        response.send(json);
+       } catch (e) {
+            console.log("not JSON");
+            response.send({"status":"error"}); 
+        }
+
+    });
+
+    //get all todos
+    app.get("/leeds",function(req,res){
+
+        db.collection('leeds').find().toArray(function(err, docs) {
+
+            assert.equal(err, null);
+            assert.notEqual(docs.length, 0);                        
+	         console.log("GET REQUEST");                              
+             res.send(docs);            
+        });
+    });
+    app.listen(3000,function () {
+        console.log("Escutando na porta 3000...");
+    });
+
+});
+
+
